3da57cbca24faefb2d43beed3137c0c8e91141e5
juunaaa <juunaaa1203@gmail.com>
2023-04-30 10:52:49 +0900

    230430

--- Level_2/230430 과제 진행하기.py
+++ Level_2/230430 과제 진행하기.py
@@ -28,5 +28,20 @@ def solution(plans):
                 work_in_progress, taken_time = paused_task.pop()
     return answer
 
+# 훨씬 더 좋은 풀이
+def solution(plans):
+    plans = sorted(map(lambda x: [x[0], int(x[1][:2]) * 60 + int(x[1][3:]), int(x[2])], plans), key=lambda x: -x[1])
+
+    lst = []
+    while plans:
+        x = plans.pop()
+        for i, v in enumerate(lst):
+            if v[0] > x[1]:
+                lst[i][0] += x[2]
+        lst.append([x[1] + x[2], x[0]])
+    lst.sort()
+
+    return list(map(lambda x: x[1], lst))
+
 a = [["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]
 print(solution(a))

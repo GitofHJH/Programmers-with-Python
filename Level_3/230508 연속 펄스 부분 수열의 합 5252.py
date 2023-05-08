7111e8f1ea50390782d36782627d20bd9db41d0a
juunaaa <juunaaa1203@gmail.com>
2023-05-08 17:50:09 +0900

    230508

---
+++ Level_3/230508 연속 펄스 부분 수열의 합 5252.py
@@ -0,0 +1,24 @@
+def solution(sequence):
+    answer = 0
+    size = len(sequence)
+    s1 = s2 = 0				# 1
+    s1_min = s2_min = 0		# 2
+    pulse = 1
+    
+    for i in range(size):
+        s1 += sequence[i] * pulse
+        s2 += sequence[i] * (-pulse)
+        
+        # 3
+        answer = max(answer, s1-s1_min, s2-s2_min)
+        
+        # 4
+        s1_min = min(s1_min, s1)
+        s2_min = min(s2_min, s2)
+        
+        # 5
+        pulse *= -1
+    return answer
+
+a = [2, 3, -6, 1, 3, -1, 2, 4]
+print(solution(a))
